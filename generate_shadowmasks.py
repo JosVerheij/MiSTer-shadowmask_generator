da9feeebd7e7a5aba41f32eb9aa9f57c8aebd338
JosVerheij <web.github@josverheij.nl>
2022-01-03 11:26:51 +0100

    working version

--- generate_shadowmasks.py
+++ generate_shadowmasks.py
@@ -1,8 +1,7 @@
-import os
 import shutil
-from pathlib import PurePath
-import sys
+from pathlib import PurePath, Path
 
+from masks import *
 
 # cur_dir = os.path.dirname(os.path.abspath(__file__))
 OUTPUT_DIRECTORY = (
@@ -13,9 +12,9 @@ OUTPUT_DIRECTORY = (
     )
 
 BRIGHTNESS_LEVELS = {
-    "dark": 4,
-    "medium": 8,
-    "light": 10,
+    "strong definition": 4,
+    "light definition": 8,
+    "extra light definition": 10,
 }
 
 FACTOR = 6.25
@@ -39,13 +38,88 @@ STRIPES = {
     "Thin RGB":         [[4, 2, 1]],
 }
 
+GRID = {
+    "Magenta-Green": [
+        [0, 0, 0, 5, 2, 0],
+        [5, 2, 0, 5, 2, 0],
+        [5, 2, 0, 0, 0, 0],
+        [5, 2, 0, 5, 2, 0],
+    ],
+    "Monochrome": [
+        [0, 0, 0, 7, 7, 0],
+        [7, 7, 0, 7, 7, 0],
+        [7, 7, 0, 0, 0, 0],
+        [7, 7, 0, 7, 7, 0],
+    ],
+    "RGB Lean": [
+        [0, 0, 0, 0, 0, 0, 4, 4, 2, 2, 1, 1],
+        [4, 4, 2, 2, 1, 1, 4, 4, 2, 2, 1, 1],
+        [4, 4, 2, 2, 1, 1, 4, 4, 2, 2, 1, 1],
+        [4, 4, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0],
+        [4, 4, 2, 2, 1, 1, 4, 4, 2, 2, 1, 1],
+        [4, 4, 2, 2, 1, 1, 4, 4, 2, 2, 1, 1],
+    ],
+    "RGB": [
+        [0, 0, 0, 0, 0, 0, 0, 4, 4, 2, 2, 1, 1, 0],
+        [4, 4, 2, 2, 1, 1, 0, 4, 4, 2, 2, 1, 1, 0],
+        [4, 4, 2, 2, 1, 1, 0, 4, 4, 2, 2, 1, 1, 0],
+        [4, 4, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 4, 2, 2, 1, 1, 0, 4, 4, 2, 2, 1, 1, 0],
+        [4, 4, 2, 2, 1, 1, 0, 4, 4, 2, 2, 1, 1, 0],
+    ],
+    "RYCB Lean": [
+        [0, 0, 0, 0, 4, 6, 3, 1],
+        [4, 6, 3, 1, 4, 6, 3, 1],
+        [4, 6, 3, 1, 0, 0, 0, 0],
+        [4, 6, 3, 1, 4, 6, 3, 1],
+    ],
+    "RYCB": [
+        [0, 0, 0, 0, 0, 4, 6, 3, 1, 0],
+        [4, 6, 3, 1, 0, 4, 6, 3, 1, 0],
+        [4, 6, 3, 1, 0, 0, 0, 0, 0, 0],
+        [4, 6, 3, 1, 0, 4, 6, 3, 1, 0],
+    ],
+    "Squished VGA": [
+        [4, 4, 2, 2, 1, 1],
+        [2, 1, 1, 4, 4, 2],
+    ],
+    "Thin RGB Lean": [
+        [0, 0, 0, 4, 2, 1],
+        [4, 2, 1, 4, 2, 1],
+        [4, 2, 1, 0, 0, 0],
+        [4, 2, 1, 4, 2, 1],
+    ],
+    "Thin RGB": [
+        [0, 0, 0, 0, 4, 2, 1, 0],
+        [4, 2, 1, 0, 4, 2, 1, 0],
+        [4, 2, 1, 0, 0, 0, 0, 0],
+        [4, 2, 1, 0, 4, 2, 1, 0],
+    ],
+    "VGA": [
+        [4, 4, 2, 2, 1, 1],
+        [4, 4, 2, 2, 1, 1],
+        [2, 1, 1, 4, 4, 2],
+        [2, 1, 1, 4, 4, 2],
+    ],
+}
+
 
 def generate_mask_files(path, name, mask_matrix, br_level):
 
     for i in range(STEPS):
-        f_name = f'{name}_Br_{100 + round(FACTOR * i)}'
+        if i % 2 != 0:  # only execute every 2nd iteration
+            continue
 
-        with open(os.path.join(path, f'{f_name}.txt'), "w") as f:
+        f_name = f'{name}'
+
+        _path = Path(path) if i == 0 else (
+            Path(path) /
+            'Brightness++' /
+            f'Brightness {100 + round(FACTOR * i)}%'
+            )
+        _path.mkdir(parents=True, exist_ok=True)
+
+        with open(PurePath(_path) / f'{f_name}.txt', "w") as f:
             f.write("v2\n\n")
             f.write(f'{len(mask_matrix[0])},{len(mask_matrix)}')
             f.write("\n\n")
@@ -64,11 +138,19 @@ def convert_matrix(matrix, on_level, off_level):
 
 
 # Delete all filters and regenerate them
-if os.path.exists(OUTPUT_DIRECTORY):
+if Path(OUTPUT_DIRECTORY).exists():
     shutil.rmtree(OUTPUT_DIRECTORY)
+
+# STRIPES
 for br_name, br_level in BRIGHTNESS_LEVELS.items():
     for mask_name, mask_matrix in STRIPES.items():
-        path = os.path.join(OUTPUT_DIRECTORY, "Stripes", br_name.title(), mask_name)
-        os.makedirs(path, exist_ok=True)
+        path = PurePath(OUTPUT_DIRECTORY) / "Stripes" / br_name.title()
+
+        generate_mask_files(path, mask_name, mask_matrix, br_level)
+
+# GRID
+for br_name, br_level in BRIGHTNESS_LEVELS.items():
+    for mask_name, mask_matrix in GRID.items():
+        path = PurePath(OUTPUT_DIRECTORY) / "Grid" / br_name.title()
 
         generate_mask_files(path, mask_name, mask_matrix, br_level)
