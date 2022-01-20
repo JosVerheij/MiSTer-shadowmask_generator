4a094e051902febf62fbd3c061ab716e29ada427
JosVerheij <web.github@josverheij.nl>
2022-01-20 21:33:15 +0100

    Revert "rm"
    This reverts commit 3baa5ff511ef3404cb2967b670aef7c85523994f.

---
+++ main.py
@@ -0,0 +1,25 @@
+import shutil
+from pathlib import Path
+
+import mask as msk
+
+
+OUTPUT_DIRECTORY = (
+    Path(__file__).parents[1] /
+    "ShadowMasks_MiSTer" /
+    "Shadow_Masks" /
+    "MyCustom Masks"
+    )
+
+if Path(OUTPUT_DIRECTORY).exists():
+    shutil.rmtree(OUTPUT_DIRECTORY)
+
+msk.set_intensity_levels(
+    [100, 110, 125, 137.5, 150],
+    [25, 37.5, 50],
+    )
+for path in sorted(Path('approved').glob('**/*.txt')):
+    msk.get_mask_file(path)
+    msk.new_mask_file(OUTPUT_DIRECTORY, 1440)
+    msk.new_mask_file(OUTPUT_DIRECTORY, 1080)
+    msk.new_mask_file(OUTPUT_DIRECTORY, 720)
